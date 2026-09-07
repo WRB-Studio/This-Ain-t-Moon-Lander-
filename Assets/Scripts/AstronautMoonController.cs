cb2f2dc6343a4366226c8424ede6fe8c31b2ead7
WRB.Studio <32596430+WRB-Studio@users.noreply.github.com>
2026-09-07 14:33:00 +0200

    Refactor astronaut gravity and EVA interaction

--- Assets/Scripts/AstronautMoonController.cs
+++ Assets/Scripts/AstronautMoonController.cs
@@ -1,26 +1,25 @@
-using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class AstronautMoonController : MonoBehaviour
 {
     [Header("Anim")]
-    [SerializeField] bool invertFlip = false;
+    [SerializeField] bool invertFlip;
     [SerializeField] float moveEpsilon = 0.0001f;
 
     [Header("Moon Hop (visual only)")]
-    Transform visualRoot;      // Sprite/Child, nicht das Rigidbody-Objekt
-    [SerializeField] float hopAmp = 0.03f;      // Weltunits, klein!
-    [SerializeField] float hopFreq = 6f;        // Hz
-    Vector3 visualBaseLocalPos;
+    [SerializeField] float hopAmp = 0.03f;
+    [SerializeField] float hopFreq = 6f;
 
     [Header("Movement")]
     [SerializeField] float maxMoveSpeed = 6f;
     [SerializeField] float rotateSmooth = 12f;
-    [Range(0f, 1f)][SerializeField] float inputDeadZone = 0.15f;
+    [Range(0f, 1f)] [SerializeField] float inputDeadZone = 0.15f;
 
-    Animator anim;
-    SpriteRenderer sr;
+    Transform visualRoot;
+    Vector3 visualBaseLocalPos;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     Transform moonCenter;
 
@@ -35,40 +34,40 @@ public class AstronautMoonController : MonoBehaviour
         rb.gravityScale = 0f;
         rb.freezeRotation = true;
 
-        sr = GetComponentInChildren<SpriteRenderer>();
-        anim = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
 
-        visualRoot = transform.GetChild(0);
-        visualBaseLocalPos = visualRoot.localPosition;
+        if (transform.childCount > 0)
+        {
+            visualRoot = transform.GetChild(0);
+            visualBaseLocalPos = visualRoot.localPosition;
+        }
     }
 
-    void Start() => CacheMoonCenter();
+    void Start()
+    {
+        moonCenter = GravityManager2D.Instance ? GravityManager2D.Instance.transform : null;
+    }
 
     void Update() => ReadInputDir();
 
     void FixedUpdate()
     {
-        if (!moonCenter) return;
+        if (!moonCenter || !GravityManager2D.Instance) return;
 
-        Vector2 toCenter = GetToMoonCenter();
+        Vector2 toCenter = (Vector2)moonCenter.position - rb.position;
         if (toCenter.sqrMagnitude < 0.0001f) return;
 
         Vector2 radialIn = toCenter.normalized;
         Vector2 radialOut = -radialIn;
         Vector2 tangent = new Vector2(-radialOut.y, radialOut.x);
 
-        ApplyMoonGravity(radialIn);
+        ApplyGravity();
         ApplyUprightRotation(radialOut);
 
         float targetTangent = CalcTargetTangentSpeed(tangent);
         ApplyTangentialVelocity(radialIn, tangent, targetTangent);
-
-        UpdateAnimAndFlip(targetTangent);
-    }
-
-    void CacheMoonCenter()
-    {
-        moonCenter = GravityManager2D.Instance ? GravityManager2D.Instance.transform : null;
+        UpdateVisuals(targetTangent);
     }
 
     void ReadInputDir()
@@ -84,33 +83,32 @@ public class AstronautMoonController : MonoBehaviour
         }
 #endif
 
-        if (Input.touchCount > 0)
-        {
-            var t = Input.GetTouch(0);
-            if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Moved || t.phase == TouchPhase.Stationary)
-                desiredDirWorld = ScreenToWorldDir(t.position);
-        }
+        if (Input.touchCount <= 0) return;
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            desiredDirWorld = ScreenToWorldDir(touch.position);
     }
 
     Vector2 ScreenToWorldDir(Vector2 screenPos)
     {
-        var wp = Camera.main.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, 0f));
-        Vector2 dir = (Vector2)wp - rb.position;
-        return dir.sqrMagnitude < 0.0001f ? Vector2.zero : dir.normalized;
+        Vector3 world = Camera.main.ScreenToWorldPoint(screenPos);
+        Vector2 direction = (Vector2)world - rb.position;
+        return direction.sqrMagnitude < 0.0001f ? Vector2.zero : direction.normalized;
     }
 
-    Vector2 GetToMoonCenter() => (Vector2)moonCenter.position - rb.position;
-
-    void ApplyMoonGravity(Vector2 radialIn)
+    void ApplyGravity()
     {
-        rb.AddForce(radialIn * GravityManager2D.Instance.moonGravityStrength, ForceMode2D.Force);
+        Vector2 gravity = GravityManager2D.Instance.GetGravityAt(rb.position);
+        if (gravity.sqrMagnitude > 0.0001f)
+            rb.AddForce(gravity * rb.mass, ForceMode2D.Force);
     }
 
     void ApplyUprightRotation(Vector2 radialOut)
     {
         float desiredZ = Mathf.Atan2(radialOut.y, radialOut.x) * Mathf.Rad2Deg - 90f;
-        float z = Mathf.LerpAngle(rb.rotation, desiredZ, 1f - Mathf.Exp(-rotateSmooth * Time.fixedDeltaTime));
-        rb.MoveRotation(z);
+        float k = 1f - Mathf.Exp(-rotateSmooth * Time.fixedDeltaTime);
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, desiredZ, k));
     }
 
     float CalcTargetTangentSpeed(Vector2 tangent)
@@ -122,67 +120,44 @@ public class AstronautMoonController : MonoBehaviour
 
     void ApplyTangentialVelocity(Vector2 radialIn, Vector2 tangent, float targetTangent)
     {
-        var v = rb.linearVelocity;
-
-        // radialen Anteil behalten
-        float radialSpeed = Vector2.Dot(v, radialIn);
-        Vector2 vRadial = radialIn * radialSpeed;
-
-        // tangentialen Anteil NICHT auf 0 resetten, sondern direkt setzen
-        Vector2 vTangent = tangent * targetTangent;
-
-        rb.linearVelocity = vRadial + vTangent;
+        Vector2 velocity = rb.linearVelocity;
+        float radialSpeed = Vector2.Dot(velocity, radialIn);
+        rb.linearVelocity = radialIn * radialSpeed + tangent * targetTangent;
     }
 
-    void UpdateAnimAndFlip(float targetTangent)
+    void UpdateVisuals(float targetTangent)
     {
-        bool isWalking = Mathf.Abs(targetTangent) > moveEpsilon;
-        if (anim) anim.SetBool(AnimIsWalking, isWalking);
+        bool walking = Mathf.Abs(targetTangent) > moveEpsilon;
+        if (animator) animator.SetBool(AnimIsWalking, walking);
 
-        if (isWalking && sr)
+        if (walking && spriteRenderer)
         {
-            // wenn Flip bei dir immer falsch ist: hier "<" zu ">" ändern ODER invertFlip nutzen
             bool flip = targetTangent > 0f;
             lastFlip = invertFlip ? !flip : flip;
         }
 
-        if (sr) sr.flipX = lastFlip;
+        if (spriteRenderer) spriteRenderer.flipX = lastFlip;
+        if (!visualRoot) return;
 
-
-        // “hop” nur beim laufen
-        if (isWalking)
-        {
-            float y = Mathf.Sin(Time.time * hopFreq) * hopAmp;
-            visualRoot.localPosition = visualBaseLocalPos + new Vector3(0f, y, 0f);
-        }
-        else
-        {
-            visualRoot.localPosition = visualBaseLocalPos;
-        }
+        float hop = walking ? Mathf.Sin(Time.time * hopFreq) * hopAmp : 0f;
+        visualRoot.localPosition = visualBaseLocalPos + new Vector3(0f, hop, 0f);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Lander")) return;
 
-        var button = MoonEVAController.Instance.btnExit;
-        button.GetComponentInChildren<TMP_Text>().text = "Enter Lander";
-        button.onClick.RemoveAllListeners();
-        button.onClick.AddListener(() => MoonEVAController.Instance.EnterLander(other.GetComponent<LanderController>()));
-        button.gameObject.SetActive(true);
-        LanderUI.Instance.SetPanelBottomCenter();
+        LanderController lander = other.GetComponentInParent<LanderController>();
+        if (lander)
+            MoonEVAController.Instance.ShowEnterLander(lander);
     }
 
-    void OnTriggerExit2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D other)
     {
-        if (!collision.CompareTag("Lander")) return;
+        if (!other.CompareTag("Lander")) return;
 
-        var button = MoonEVAController.Instance.btnExit;
-        button.GetComponentInChildren<TMP_Text>().text = "Exit Lander";
-        button.onClick.RemoveAllListeners();
-        button.onClick.AddListener(() => MoonEVAController.Instance.ExitLander());
-        button.gameObject.SetActive(false);
-        LanderUI.Instance.SetPanelBottomCenter();
+        LanderController lander = other.GetComponentInParent<LanderController>();
+        if (lander)
+            MoonEVAController.Instance.HideEnterLander(lander);
     }
-
 }
